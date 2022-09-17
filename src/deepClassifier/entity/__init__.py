220eac9811ebf1d9ed167e85bb1aaf774b339752
Sourav9827 <souravmandal9827@gmail.com>
2022-09-17 12:30:47 +0530

    deep imports for DI

--- src/deepClassifier/entity/__init__.py
+++ src/deepClassifier/entity/__init__.py
@@ -1,8 +1,1 @@
-from collections import namedtuple
-
-DataIngestionConfig = namedtuple("DataIngestionConfig",[
-    "root_dir",
-    "source_URL",
-    "local_data_file",
-    "unzip_dir"
-])
+from deepClassifier.entity.config_entity import *
